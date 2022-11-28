50d4e2f342d00a44842a38974e0111c91edf03f9
faazizpro <fahadafs11@gmail.com>
2022-11-29 00:33:01 +0600

    Google Fonts Installed

---
+++ src/Pages/Simple.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Simple = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-poppins font-bold '>This is Heading One for font one</h1>
+            <h1 className='text-5xl mt-5 font-poppins font-semibold'>This is Fahad</h1>
+        </div>
+    );
+};
+
+export default Simple;
