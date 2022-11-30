011e7820c71eeec9d74712963323102ab3515fa8
faazizpro <fahadafs11@gmail.com>
2022-11-30 16:58:58 +0600

    Testimonial Section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './components/About/About';
 import Course from './components/Course/Course';
 import Header from './components/Header/Header';
 import Highlight from './components/Highlight/Highlight';
+import Testimonial from './components/Testimonial/Testimonial';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Highlight></Highlight>
         <About></About>
         <Course></Course>
+        <Testimonial></Testimonial>
     </div>
   );
 }
